9905ab077ed45ef1fbf52ce59592cd592ab4d2b1
LeSa <le.salang@yandex.ru>
2024-07-18 01:14:28 +0400

    Create network module

---
+++ network/src/main/java/com/lesa/network/Api.kt
@@ -0,0 +1,59 @@
+package com.lesa.network
+
+import com.skydoves.retrofit.adapters.result.ResultCallAdapterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * [API Documentation](https://freecurrencyapi.com/docs/)
+ */
+interface Api {
+    @GET(GET_LATEST_EXCHANGE_RATES)
+    suspend fun getExchangeRates(
+        @Query("base_currency") baseCurrency: String
+    )
+}
+
+fun createApi(
+    baseUrl: String,
+    apiKey: String,
+    okHttpClient: OkHttpClient? = null,
+): Api {
+    val json = Json { ignoreUnknownKeys = true }
+
+    return retrofit(
+        baseUrl = baseUrl,
+        apiKey = apiKey,
+        okHttpClient = okHttpClient,
+        json = json
+    ).create()
+}
+
+private fun retrofit(
+    baseUrl: String,
+    apiKey: String,
+    okHttpClient: OkHttpClient?,
+    json: Json = Json
+): Retrofit {
+    val jsonConverter: Converter.Factory = json.asConverterFactory("application/json".toMediaType())
+
+    val modifiedOkHttpClient = (okHttpClient?.newBuilder() ?: OkHttpClient.Builder())
+        .addInterceptor(ApiKeyInterceptor(apikey = apiKey))
+        .build()
+
+    return Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(jsonConverter)
+        .addCallAdapterFactory(ResultCallAdapterFactory.create())
+        .client(modifiedOkHttpClient)
+        .build()
+}
+
+const val GET_LATEST_EXCHANGE_RATES = "latest"
---
+++ network/src/main/java/com/lesa/network/ApiKeyInterceptor.kt
@@ -0,0 +1,20 @@
+package com.lesa.network
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+internal class ApiKeyInterceptor(private val apikey: String) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+            .newBuilder()
+            .url(
+                chain.request()
+                    .url
+                    .newBuilder()
+                    .addQueryParameter("apikey", apikey)
+                    .build()
+            )
+            .build()
+        return chain.proceed(request)
+    }
+}
